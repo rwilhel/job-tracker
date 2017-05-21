8e4332d20c16ade260f1f6c7b745e84f4557fb28
Rhonda <rwilhel@gmail.com>
2017-05-21 14:40:53 -0600

    Completes category functionality section

---
+++ spec/features/categories/user_sees_one_category_spec.rb
@@ -0,0 +1,17 @@
+require 'rails_helper'
+
+describe "User sees a specific category" do
+  scenario "a user sees all jobs associated with the category" do
+    category = create(:category_with_jobs)
+
+    visit category_path(category)
+
+    expect(page).to have_content(category.title)
+    expect(page).to have_content("Jobs")
+
+    category.jobs.each do |job|
+      expect(page).to have_link job.title, company_job_path(job.company, job)
+    end
+        save_and_open_page
+  end
+end
